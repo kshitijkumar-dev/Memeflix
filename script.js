e77b37c092d69a5745d3b3b9fc36c638a44a2432
kshitijkumar-dev <kshitijkumar.dev@gmail.com>
2026-02-01 00:34:34 +0530

    Update for adding download button to script.js

--- script.js
+++ script.js
@@ -3,34 +3,39 @@ const memeImg = document.getElementById("memeImg");
 const memeTitle = document.getElementById("memeTitle");
 const loading = document.getElementById("loading");
 const themeBtn = document.getElementById("themeBtn");
+const downloadBtn = document.getElementById("downloadBtn");
 
-//function for generation of meme
-async function getMeme() {
+
+//function start
+async function getMeme(){
   loading.style.display = "block";
   memeImg.style.display = "none";
 
-  try {
+  try{
     const response = await fetch("https://meme-api.com/gimme");
     const data = await response.json();
 
     memeImg.src = data.url;
     memeTitle.innerText = data.title;
+    downloadBtn.href = data.url;
 
-    memeImg.onload = () => {
+    memeImg.onload = ()=>{
       loading.style.display = "none";
       memeImg.style.display = "block";
-    };
+    }
 
-  } catch {
-    loading.innerText = "Failed to load meme ;-; ";
+  }catch{
+    loading.innerText = "Failed to load meme 😢";
   }
 }
-//end of function
+//function end
 
-//for dark theme
-themeBtn.addEventListener("click", () => {
+
+btn.addEventListener("click", getMeme);
+
+themeBtn.addEventListener("click", ()=>{
   document.body.classList.toggle("dark");
 });
 
-// Load first meme as soon as website opens
+//for generqation of meme as soon as website opens
 getMeme();
